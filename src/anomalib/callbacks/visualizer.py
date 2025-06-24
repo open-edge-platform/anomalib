26e65474ee94981b7b2397f02d15ff3f07054a19
samet-akcay <samet.akcay@intel.com>
2025-06-24 20:42:31 +0100

    refactor: standardize variable naming conventions across the codebase
    This commit updates various files to replace underscore-prefixed variable names with more descriptive names, enhancing code readability and consistency. Changes include renaming variables in the Engine, AUPRO metric, model initialization, and several test files. This refactor aims to improve clarity and maintainability of the code.
    
    Signed-off-by: samet-akcay <samet.akcay@intel.com>

--- src/anomalib/callbacks/visualizer.py
+++ src/anomalib/callbacks/visualizer.py
@@ -270,9 +270,9 @@ class _VisualizationCallback(Callback):
             # check if logger object is same as the requested object
             if isinstance(available_loggers[log_to], ImageLoggerBase):
                 logger: ImageLoggerBase = cast("ImageLoggerBase", available_loggers[log_to])  # placate mypy
-                _name = filename.parent.name + "_" + filename.name if isinstance(filename, Path) else filename
+                name = filename.parent.name + "_" + filename.name if isinstance(filename, Path) else filename
                 logger.add_image(
                     image=image,
-                    name=_name,
+                    name=name,
                     global_step=module.global_step,
                 )
