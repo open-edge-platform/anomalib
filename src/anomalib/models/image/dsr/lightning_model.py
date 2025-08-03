e4a965210f57be6e368a93a01f2f70d619adbe6f
Aimira Baitieva <63813435+abc-125@users.noreply.github.com>
2025-08-03 12:57:42 +0200

    updated year in the copyright notice

--- src/anomalib/models/image/dsr/lightning_model.py
+++ src/anomalib/models/image/dsr/lightning_model.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2024 Intel Corporation
+# Copyright (C) 2023-2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """DSR - A Dual Subspace Re-Projection Network for Surface Anomaly Detection.
