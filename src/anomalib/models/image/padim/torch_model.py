304f55544fd7af3515a13bcb78a4bbe96c75b1e4
Alfie Roddan <51797647+alfieroddan@users.noreply.github.com>
2025-08-26 09:58:17 +0100

    padim model to now use cat instead of list
    Signed-off-by: Alfie Roddan <51797647+alfieroddan@users.noreply.github.com>

--- src/anomalib/models/image/padim/torch_model.py
+++ src/anomalib/models/image/padim/torch_model.py
@@ -147,7 +147,7 @@ class PadimModel(nn.Module):
         self.anomaly_map_generator = AnomalyMapGenerator()
 
         self.gaussian = MultiVariateGaussian()
-        self.memory_bank = torch.empty(0)
+        self.memory_bank = []
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor | InferenceBatch:
         """Forward-pass image-batch (N, C, H, W) into model to extract features.
@@ -183,11 +183,7 @@ class PadimModel(nn.Module):
             embeddings = self.tiler.untile(embeddings)
 
         if self.training:
-            if self.memory_bank.size(0) == 0:
-                self.memory_bank = embeddings
-            else:
-                new_bank = torch.cat((self.memory_bank, embeddings), dim=0).to(self.memory_bank)
-                self.memory_bank = new_bank
+            self.memory_bank.append(embeddings)
             return embeddings
 
         anomaly_map = self.anomaly_map_generator(
@@ -234,6 +230,8 @@ class PadimModel(nn.Module):
         Raises:
             ValueError: If the memory bank is empty.
         """
+        self.memory_bank = torch.vstack(self.memory_bank)
+
         if self.memory_bank.size(0) == 0:
             msg = "Memory bank is empty. Cannot perform coreset selection."
             raise ValueError(msg)
@@ -242,4 +240,4 @@ class PadimModel(nn.Module):
         self.gaussian.fit(self.memory_bank)
 
         # clear memory bank, redcues gpu usage
-        self.memory_bank = torch.empty(0).to(self.memory_bank)
+        self.memory_bank = torch.empty(0)
