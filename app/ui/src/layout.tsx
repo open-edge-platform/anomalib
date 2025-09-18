dd8ce5a88cc21984017248ee566a22e2abbf19cc
Mark Redeman <markredeman@gmail.com>
2025-09-18 09:46:24 +0200

    Update navbar title

--- app/ui/src/layout.tsx
+++ app/ui/src/layout.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <View backgroundColor={'gray-300'} gridArea={'header'}>
             <Flex height='100%' alignItems={'center'} marginX='1rem' gap='size-200'>
-                <View marginEnd='size-200'>
+                <View marginEnd='size-200' maxWidth={'5ch'}>
                     <span>Geti Inspect</span>
                 </View>
             </Flex>
