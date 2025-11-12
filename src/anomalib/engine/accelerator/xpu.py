90ff4ec71ad7f5a51ff71eebab02a965e70a8380
Niclas <152474825+waschsalz@users.noreply.github.com>
2025-11-12 11:07:07 +0100

    Update xpu.py with docstring
    Signed-off-by: Niclas <152474825+waschsalz@users.noreply.github.com>

--- src/anomalib/engine/accelerator/xpu.py
+++ src/anomalib/engine/accelerator/xpu.py
@@ -14,6 +14,7 @@ class XPUAccelerator(Accelerator):
     
     @property
     def name(self) -> str:
+        """Setting the name of the accelerator which is required for accelerators by pytorch-lightning >= 2.5.6"""
         return "xpu"
     
     @staticmethod
