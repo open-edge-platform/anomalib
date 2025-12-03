d7055700775692a1d179cd232e3394266d938cec
Colorado, Camilo <camilo.colorado@intel.com>
2025-12-03 09:49:54 +0100

    rename project
    Signed-off-by: Colorado, Camilo <camilo.colorado@intel.com>

--- application/ui/src/features/inspect/projects-management/project-list-item/project-list-item.test.tsx
+++ application/ui/src/features/inspect/projects-management/project-list-item/project-list-item.test.tsx
@@ -1,6 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { HttpResponse } from 'msw';
 import { useNavigate } from 'react-router';
+import { http } from 'src/api/utils';
+import { server } from 'src/msw-node-setup';
 import { vi } from 'vitest';
 
 import { useWebRTCConnection } from '../../../../components/stream/web-rtc-connection-provider';
@@ -37,11 +41,51 @@ describe('ProjectListItem', () => {
     });
 
     it('navigates to project when clicked', async () => {
-        render(<ProjectListItem project={mockProject} isInEditMode={false} onBlur={vi.fn()} isActive={false} />);
+        render(
+            <QueryClientProvider client={new QueryClient()}>
+                <ProjectListItem
+                    project={mockProject}
+                    isInEditMode={false}
+                    isActive={false}
+                    setProjectInEdition={vi.fn()}
+                />
+            </QueryClientProvider>
+        );
 
         await userEvent.click(screen.getByRole('listitem'));
 
         expect(mockStop).toHaveBeenCalled();
         expect(mockNavigate).toHaveBeenCalledWith('/projects/project-123?mode=Dataset');
+    });
+
+    it('updates project name when edited', async () => {
+        const updateNameSpy = vi.fn();
+        const mockedSetProjectInEdition = vi.fn();
+
+        server.use(
+            http.patch('/api/projects/{project_id}', () => {
+                updateNameSpy();
+                return HttpResponse.json({});
+            })
+        );
+
+        render(
+            <QueryClientProvider client={new QueryClient()}>
+                <ProjectListItem
+                    project={mockProject}
+                    isActive={true}
+                    isInEditMode={true}
+                    setProjectInEdition={mockedSetProjectInEdition}
+                />
+            </QueryClientProvider>
+        );
+
+        const input = screen.getByRole('textbox', { name: /edit project name/i });
+        await userEvent.clear(input);
+        await userEvent.type(input, 'Updated Project Name');
+        await userEvent.tab();
+
+        expect(updateNameSpy).toHaveBeenCalled();
+        expect(mockedSetProjectInEdition).toHaveBeenCalledWith(null);
     });
 });
