164c8ec074657fdc2b21d0db1f6e257272a53281
Max Xiang <xiangxiang.ma@intel.com>
2025-12-04 22:47:54 +0100

    camera unique names and auto name completion
    Signed-off-by: Max Xiang <xiangxiang.ma@intel.com>

--- application/ui/src/features/inspect/toolbar/sources/source-list/settings-list/settings-list.component.tsx
+++ application/ui/src/features/inspect/toolbar/sources/source-list/settings-list/settings-list.component.tsx
@@ -11,7 +11,7 @@ interface SettingsListProps {
 const CameraDeviceDisplay = ({ deviceId }: { deviceId: number }) => {
     const { data: cameraDevices, isLoading } = $api.useQuery('get', '/api/devices/camera');
     const devices = cameraDevices?.devices ?? [];
-    const device = devices[deviceId];
+    const device = devices.find((cam) => cam.index === deviceId);
 
     if (isLoading) {
         return <span>Loading...</span>;
