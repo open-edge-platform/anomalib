8bf3567e5cb424db72ebf7bf9e65317ec4f480f0
Colorado, Camilo <camilo.colorado@intel.com>
2025-12-12 12:19:30 +0100

    apply aspect ratio on inference img
    Signed-off-by: Colorado, Camilo <camilo.colorado@intel.com>

--- application/ui/src/features/inspect/dataset/media-preview/inference-result/inference-result.component.tsx
+++ application/ui/src/features/inspect/dataset/media-preview/inference-result/inference-result.component.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { SchemaPredictionResponse } from '@geti-inspect/api/spec';
-import { dimensionValue, Flex, View } from '@geti/ui';
+import { DimensionValue, Responsive, View } from '@geti/ui';
 import { clsx } from 'clsx';
 import { AnimatePresence, motion } from 'motion/react';
 import { isNonEmptyString } from 'src/features/inspect/utils';
@@ -9,6 +9,7 @@ import { isNonEmptyString } from 'src/features/inspect/utils';
 import { MediaItem } from '../../types';
 import { useInference } from '../providers/inference-opacity-provider.component';
 import { LabelScore } from './label-score.component';
+import { getImageDimensions } from './util';
 
 import classes from './inference-result.module.scss';
 
@@ -17,49 +18,52 @@ interface InferenceResultProps {
     inferenceResult: SchemaPredictionResponse | undefined;
 }
 
+const labelHeight: Responsive<DimensionValue> = 'size-350';
+
 export const InferenceResult = ({ selectedMediaItem, inferenceResult }: InferenceResultProps) => {
     const { inferenceOpacity } = useInference();
-    const [isVerticalImage, setIsVerticalImage] = useState(false);
+    const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0, left: 0, top: 0 });
 
-    const handleImageOrientation = (imageElement: HTMLImageElement) => {
-        setIsVerticalImage(imageElement.clientHeight > imageElement.clientWidth);
+    const handleImageLoaded = (imageElement: HTMLImageElement) => {
+        setImageDimensions(getImageDimensions(imageElement));
     };
 
     return (
-        <Flex height={'100%'} direction={'column'} alignItems={'center'} justifyContent={'center'}>
-            <Flex height={'100%'} direction={'column'} alignItems={'baseline'} justifyContent={'center'}>
-                <View height={'size-350'}>
-                    {inferenceResult && <LabelScore label={inferenceResult.label} score={inferenceResult.score} />}
-                </View>
-
+        <>
+            {inferenceResult && (
                 <View
-                    position={'relative'}
-                    UNSAFE_style={{ height: isVerticalImage ? `calc(100% - ${dimensionValue('size-350')})` : 'auto' }}
+                    height={labelHeight}
+                    position={'absolute'}
+                    maxWidth={'size-1600'}
+                    top={imageDimensions.top}
+                    left={imageDimensions.left}
                 >
-                    <img
-                        alt={selectedMediaItem.filename}
-                        className={clsx(classes.img, { [classes.verticalImg]: isVerticalImage })}
-                        src={`/api/projects/${selectedMediaItem.project_id}/images/${selectedMediaItem.id}/full`}
-                        onLoad={({ target }) => handleImageOrientation(target as HTMLImageElement)}
-                    />
-
-                    <AnimatePresence>
-                        {isNonEmptyString(inferenceResult?.anomaly_map) && (
-                            <>
-                                <motion.img
-                                    exit={{ opacity: 0 }}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: inferenceOpacity }}
-                                    src={`data:image/png;base64,${inferenceResult.anomaly_map}`}
-                                    alt={`${selectedMediaItem.filename} inference`}
-                                    className={clsx(classes.inferenceImage)}
-                                    style={{ opacity: inferenceOpacity }}
-                                />
-                            </>
-                        )}
-                    </AnimatePresence>
+                    <LabelScore label={inferenceResult.label} score={inferenceResult.score} />
                 </View>
-            </Flex>
-        </Flex>
+            )}
+
+            <View width={'100%'} height={'100%'} position={'relative'} marginTop={labelHeight}>
+                <img
+                    alt={selectedMediaItem.filename}
+                    className={clsx(classes.img)}
+                    src={`/api/projects/${selectedMediaItem.project_id}/images/${selectedMediaItem.id}/full`}
+                    onLoad={({ target }) => handleImageLoaded(target as HTMLImageElement)}
+                />
+
+                <AnimatePresence>
+                    {isNonEmptyString(inferenceResult?.anomaly_map) && (
+                        <motion.img
+                            exit={{ opacity: 0 }}
+                            style={imageDimensions}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: inferenceOpacity }}
+                            className={clsx(classes.inferenceImage)}
+                            src={`data:image/png;base64,${inferenceResult.anomaly_map}`}
+                            alt={`${selectedMediaItem.filename} inference`}
+                        />
+                    )}
+                </AnimatePresence>
+            </View>
+        </>
     );
 };
