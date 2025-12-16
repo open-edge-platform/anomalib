b11746b230eb404faa603fc66981c4b545880ade
Mark Redeman <mark.s.redeman@intel.com>
2025-12-16 17:09:59 +0100

    Move `set_start_method` to main block
    Signed-off-by: Mark Redeman <mark.s.redeman@intel.com>

--- application/backend/src/main.py
+++ application/backend/src/main.py
@@ -2,10 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import multiprocessing as mp
-
-if mp.get_start_method(allow_none=True) != "spawn":
-    mp.set_start_method("spawn", force=True)
-
 import os
 
 import uvicorn
@@ -68,6 +64,9 @@ app.include_router(snapshot_router)
 
 
 if __name__ == "__main__":
+    if mp.get_start_method(allow_none=True) != "spawn":
+        mp.set_start_method("spawn", force=True)
+
     settings = get_settings()
     uvicorn_port = int(os.environ.get("HTTP_SERVER_PORT", settings.port))
     uvicorn.run("main:app", loop="uvloop", host=settings.host, port=uvicorn_port, log_config=None)
