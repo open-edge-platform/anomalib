70921fc21f1fec3293e33288134859d91e8255ff
Max Xiang <xiangxiang.ma@intel.com>
2026-01-14 18:49:22 +0100

    fix copyright header
    Signed-off-by: Max Xiang <xiangxiang.ma@intel.com>

--- application/ui/src/features/inspect/jobs/log-entry.component.tsx
+++ application/ui/src/features/inspect/jobs/log-entry.component.tsx
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Intel Corporation
+// Copyright (C) 2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 import { Flex, Text, Tooltip, TooltipTrigger, View } from '@geti/ui';
