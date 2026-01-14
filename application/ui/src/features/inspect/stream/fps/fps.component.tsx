879d9595439871c89d72becb65df9b1577a026e7
Max Xiang <xiangxiang.ma@intel.com>
2026-01-14 08:56:52 +0100

    run prettier
    Signed-off-by: Max Xiang <xiangxiang.ma@intel.com>

--- application/ui/src/features/inspect/stream/fps/fps.component.tsx
+++ application/ui/src/features/inspect/stream/fps/fps.component.tsx
@@ -10,14 +10,13 @@ interface FpsProp {
     projectId: string;
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0
-});
-
 export const Fps = ({ projectId }: FpsProp) => {
     const { data: pipeline } = usePipeline();
     const isRunning = pipeline?.status === 'running';
+    const formatter = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: 0,
+        minimumFractionDigits: 0,
+    });
 
     const { data: metrics } = $api.useQuery(
         'get',
