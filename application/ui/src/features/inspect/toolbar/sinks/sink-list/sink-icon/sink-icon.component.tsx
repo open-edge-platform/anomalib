5646d6110b1d9987d0ae0416ffa86d4e631bf1ff
Max Xiang <xiangxiang.ma@intel.com>
2026-01-22 10:12:52 +0100

    fix style
    Signed-off-by: Max Xiang <xiangxiang.ma@intel.com>

--- application/ui/src/features/inspect/toolbar/sinks/sink-list/sink-icon/sink-icon.component.tsx
+++ application/ui/src/features/inspect/toolbar/sinks/sink-list/sink-icon/sink-icon.component.tsx
@@ -1,7 +1,7 @@
 import { Folder, Mqtt, Webhook } from '@geti-inspect/icons';
 
 interface SinkIconProps {
-    type: 'folder' | 'mqtt' | 'webhook' | 'ros' | 'disconnected';
+    type: 'folder' | 'mqtt' | 'webhook' | 'disconnected';
 }
 
 export const SinkIcon = ({ type }: SinkIconProps) => {
