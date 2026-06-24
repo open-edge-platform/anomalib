89d170976bca9fe1ffdf242ae8ef872db431a651
Alexander Riedel <alex.riedel@googlemail.com>
2026-06-24 21:48:50 +0200

    Update src/anomalib/models/image/super_add/lightning_model.py
    Co-authored-by: Samet Akcay <samet.akcay@intel.com>
    Signed-off-by: Alexander Riedel <alex.riedel@googlemail.com>

--- src/anomalib/models/image/super_add/lightning_model.py
+++ src/anomalib/models/image/super_add/lightning_model.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022-2025 Intel Corporation
+# Copyright (C) 2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """SuperADD: PatchCore-style anomaly detection on a DINOv3 backbone.
