77ed7e8a676b92b46ce37097a0667f9856047008
alexriedel1 <alex.riedel@googlemail.com>
2026-07-13 11:21:48 +0200

    deprecate dino folder

--- src/anomalib/models/components/dinov2/__init__.py
+++ src/anomalib/models/components/dinov2/__init__.py
@@ -11,3 +11,13 @@ used by the anomaly-detection models. DINOv2 backbones themselves are loaded via
 References:
     https://github.com/facebookresearch/dinov2/blob/main/dinov2/
 """
+
+import warnings
+
+warnings.warn(
+    "The anomalib.models.components.dinov2 package is deprecated and will be removed in a future release. "
+    "Please use the timm-based feature extractor instead: "
+    "anomalib.models.components.feature_extractors.TimmFeatureExtractor",
+    FutureWarning,
+    stacklevel=2,
+)
